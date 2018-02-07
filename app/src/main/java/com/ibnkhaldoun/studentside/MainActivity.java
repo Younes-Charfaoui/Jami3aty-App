a92b54f5beec94664540e6a8446bb3bd7d15cad9
Younes Charfaoui <mxcsyounes@gmail.com>
2018-02-07 22:28:22 +0100

    First Commit

---
+++ app/src/main/java/com/ibnkhaldoun/studentside/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ibnkhaldoun.studentside;
+
+import android.content.AsyncTaskLoader;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
